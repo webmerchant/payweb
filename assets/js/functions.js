6ec5f56496d367bb8ccb0e3b025dc444639b21b4
webmerchant <65113029+webmerchant@users.noreply.github.com>
2020-05-11 18:34:26 +0530

    Add files via upload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,11 @@
+// PayWeb Functions
+
 var hash_link = location.hash;
 
 if(hash_link == '#message-sent'){
    $("#message-sent").modal();
+}
+
+if(hash_link == '#quote-sent'){
+   $("#quote-sent").modal();
 }
